3ac3e3ef2d6d2c9fa1573724fce78dbbb2184bec
uzumismail <ismailluzum@gmail.com>
2025-04-08 20:16:27 +0200

    lab02-added

---
+++ labs/02-Analyze-data-with-sql/sql-scripts/SQL script 3.sql
@@ -0,0 +1,5 @@
+SELECT YEAR(OrderDate) AS OrderYear,
+       SUM((UnitPrice * Quantity) + TaxAmount) AS GrossRevenue
+FROM dbo.orders
+GROUP BY YEAR(OrderDate)
+ORDER BY OrderYear;
---
+++ labs/02-Analyze-data-with-sql/sql-scripts/Sales JSON query.sql
@@ -0,0 +1,12 @@
+
+SELECT JSON_VALUE(Doc, '$.SalesOrderNumber') AS OrderNumber,
+       JSON_VALUE(Doc, '$.CustomerName') AS Customer,
+       Doc
+FROM
+    OPENROWSET(
+        BULK 'https://datalakezjxue2n.dfs.core.windows.net/files/sales/json/**',
+        FORMAT = 'CSV',
+        FIELDTERMINATOR ='0x0b',
+        FIELDQUOTE = '0x0b',
+        ROWTERMINATOR = '0x0b'
+    ) WITH (Doc NVARCHAR(MAX)) as rows  
